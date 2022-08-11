07cf329427ee5c3bfb806e5770fe36efe8cb8300
Mohasin Patel <mohasinpatel313@gmail.com>
2022-08-11 14:20:33 +0530

    Add files via upload

---
+++ 3 Assignment/03_pr.c
@@ -0,0 +1,46 @@
+/**
+ * @file 03_pr.c
+ * @author Mohasin Patel
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-16
+ * 
+ * @problem statement :- Write a program which accepts number from the user and print odd factors of
+ *                       that number.
+ * 
+ */
+
+#include <stdio.h>
+
+void displayFactor(int);
+
+int main()
+{
+
+    int iVal = 0;
+    printf("Enter number :-\n");
+    scanf("%d", &iVal);
+
+    displayFactor(iVal);
+
+    return 0;
+}
+
+void displayFactor(int iNum)
+{
+    int iCnt = 1;
+
+    if (iNum<0)
+    {
+        iNum = -(iNum);
+    }
+    
+    while (iCnt <= iNum)
+    {
+        if (iNum % iCnt == 0 && iCnt % 2 != 0)
+        {
+            printf("%d ", iCnt);
+        }
+        iCnt++;
+    }
+}
